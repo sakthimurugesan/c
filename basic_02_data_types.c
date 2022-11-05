1d8cbc33472f1b80ea77b578a05ddff5e21e15d6
SAKTHI M <msakthi150@gmail.com>
2022-11-05 22:57:52 +0530

    /* removed

--- basic_02_data_types.c
+++ basic_02_data_types.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int main(){/*
+int main(){
 int a=12;
 float f=12.523f;
 char s[10]="sakthi";
 char b='A';
 printf("int a=\t%d\nfloat f=\t%.0f\nchar s[10]=\t%s\n",a,f,s);
 printf("char b='A'\nb\t%d\n%c",b,b);
-*/
+
 int a=0xA;
 printf("%d",a);
 return 0;
